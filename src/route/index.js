940a479c697894ca30dd7883aa352c11ca3042ae
Diana P. <dianasofiapancorbo@gmail.com>
2024-05-09 02:45:00 -0400

    hi

--- src/route/index.js
+++ src/route/index.js
@@ -9,6 +9,22 @@ router.get("/dashboard", (req, res) => {
     res.render("./pages/dashboard");
 });
 
+router.get("/MizerKids", (req, res) => { 
+    res.render("./pages/MizerKids");
+});
+
+router.get("/MizerBenders", (req, res) => { 
+    res.render("./pages/MizerBenders");
+});
+
+router.get("/MizerKids-Pulpo", (req, res) => { 
+    res.render("./pages/MizerKids-Pulpo");
+});
+
+router.get("/Pedrito", (req, res) => { 
+    res.render("./pages/Pedrito");
+});
+
 router.get("/report", (req, res) => { 
     res.render("report");
 });
